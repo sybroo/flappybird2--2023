39247d9ac44b73c3f1cce0e1d23479c6ecdca229
Syb Scholing <74767501+sybroo@users.noreply.github.com>
2022-08-25 08:47:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var gravity, Ball1
+
+class Ball{
+  constructor(x, y, w, h, vy){
+  this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vy = vy;
+  }
+  drawBall() {
+    fill("yellow");
+
+
+    this.vy += gravity;
+  }
+}
+
+function setup() {
+  createCanvas(600, 400);
+  gravity = 0.25;
+  Ball1 = new Ball(250, 200, 20, 20, 0);
+}
+
+function draw() {
+  background(225);
+  
+  Ball1.drawBall();
+}
