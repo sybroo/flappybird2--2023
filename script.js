c28f9e9525a3fa1ce6a37a22cfa7b0ce85ac68f0
Syb Scholing <74767501+sybroo@users.noreply.github.com>
2022-09-14 10:45:46 +0000

    menu gravity

--- script.js
+++ script.js
@@ -5,82 +5,95 @@ let img;
 function preload() {
   img = loadImage('menu.jpg');
 }
-  class Ball {
-    constructor(x, y, w, h, vy, c) {
-      this.x = x;
-      this.y = y;
-      this.width = w;
-      this.height = h;
-      this.vy = vy;
-      this.color = c;
-    }
-
-    drawBall() {
-      fill(this.color)
-      ellipse(this.x, this.y, this.width, this.height);
-
-
-      this.vy += gravity;
-      if (this.y < 390) {
-        this.y = this.y + this.vy;
-      }
-
-    }
+class Ball {
+  constructor(x, y, w, h, vy, c) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vy = vy;
+    this.color = c;
   }
 
-  function setup() {
-    createCanvas(700, 400);
-    gravity = 0.25;
-    Ball1 = new Ball(250, 200, 20, 20, 0, "red");
-  }
+  drawBall() {
+    fill(this.color)
+    ellipse(this.x, this.y, this.width, this.height);
 
-  function draw() {
-    if (gameState == 0) {
-      menu();
+
+    this.vy += gravity;
+
+    this.y += this.vy;
+    if (this.y > 390) {
+      this.vy = 0;
+      this.y = 390;
     }
 
-    if (gameState == 1) {
-      game();
-    }
-
-    if (gameState == 2) {
-      gameover();
+    if (this.y < 0) {
+      this.vy = 0;
+      this.y = 0;
     }
   }
+}
 
-  function menu() {
-    image(img,0, 0, width, height);
-    text("PRESS ENTER TO PLAY", 240, 200)
-    text("WELCOME!", 280, 100)
-    text("MADE BY SYB", 280, 325)
-    textSize(18);
-  }
-  function game() {
-    background(225);
-    Ball1.drawBall();
-  }
 
-  function gameover() {
-    background('black')
-    text("Press Esc to go to main menu", 150, 200)
-    text("GAME OVER", 150, 100)
-    text("gemaakt door Syb", 150, 325)
+
+function setup() {
+  createCanvas(700, 400);
+  gravity = 0.25;
+  Ball1 = new Ball(250, 200, 20, 20, 0, "red");
+}
+
+function draw() {
+  if (gameState == 0) {
+    menu();
   }
 
+  if (gameState == 1) {
+    game();
+  }
 
-  function keyPressed() {
-    (keyCode);
-    if (keyCode == 32) {
-      Ball1.vy = Ball1.vy - 5;
+  if (gameState == 2) {
+    gameover();
+  }
+}
+
+
+
+
+function menu() {
+  fill("black")
+  image(img, 0, 0, width, height);
+  text("PRESS ENTER TO PLAY", 240, 200)
+  text("WELCOME!", 280, 100)
+  text("MADE BY SYB", 280, 325)
+  textSize(18);
+}
+function game() {
+  background(225);
+  Ball1.drawBall();
+
+
+function gameover() {
+  background('black')
+  text("Press Esc to go to main menu", 150, 200)
+  text("GAME OVER", 150, 100)
+  text("gemaakt door Syb", 150, 325)
+}
+
+
+function keyPressed() {
+  (keyCode);
+  if (keyCode == 32) {
+    Ball1.vy = Ball1.vy - 5;
+  }
+
+  if (keyCode == 27) {
+    gameState = 0;
+  }
+  if (keyCode == 13) {
+    if (gameState != 1) {
+      ball1 = new Ball(250, 200, 20, 20, 0);
+      gameState = 1;
     }
-
-    if (keyCode == 27) {
-      gameState = 0;
-    }
-    if (keyCode == 13) {
-      if (gameState != 1) {
-        ball1 = new Ball(250, 200, 20, 20, 0);
-        gameState = 1;
-      }
-    }
   }
+}
